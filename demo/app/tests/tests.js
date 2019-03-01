0692882df0c0d6761a43d85ffb657846180a7cdc
Brad Martin <bradwaynemartin@gmail.com>
2019-03-01 17:41:52 -0600

    tests and travis

---
+++ demo/app/tests/tests.js
@@ -0,0 +1,21 @@
+describe('imagefilters class', function() {
+  it('can be instantiated', function() {
+    var testImageFilters;
+    var ImageFilters = require('nativescript-image-filters').ImageFilters;
+    if (ImageFilters) {
+      testImageFilters = new ImageFilters();
+      console.log('ImageFilters instance: ' + testImageFilters);
+    }
+
+    expect(function() {
+      return new ImageFilters();
+    }).not.toThrow();
+
+    expect(function() {
+      var x = new ImageFilters();
+      x.boost();
+    }).toThrow();
+
+    expect(new ImageFilters()).toBeDefined();
+  });
+});
